6dac6fdeb3f9fe9cb914bb416cc6874b5a48463d
BorreroRalph <145305769+BorreroRalph@users.noreply.github.com>
2023-11-30 19:58:41 +0800

    updated

--- FiberView/FiberView/Form1.Designer.cs
+++ FiberView/FiberView/Form1.Designer.cs
@@ -29,13 +29,51 @@ namespace FiberView
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Next = new System.Windows.Forms.Button();
+            this.schedule = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.schedule)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Next
+            // 
+            this.Next.Location = new System.Drawing.Point(114, 282);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(101, 48);
+            this.Next.TabIndex = 1;
+            this.Next.Text = "View";
+            this.Next.UseVisualStyleBackColor = true;
+            // 
+            // schedule
+            // 
+            this.schedule.AllowUserToAddRows = false;
+            this.schedule.AllowUserToDeleteRows = false;
+            this.schedule.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.schedule.Location = new System.Drawing.Point(420, 57);
+            this.schedule.Name = "schedule";
+            this.schedule.ReadOnly = true;
+            this.schedule.RowHeadersWidth = 51;
+            this.schedule.RowTemplate.Height = 24;
+            this.schedule.Size = new System.Drawing.Size(500, 478);
+            this.schedule.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(952, 581);
+            this.Controls.Add(this.schedule);
+            this.Controls.Add(this.Next);
+            this.Name = "Form1";
+            this.Text = "FiberView";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.schedule)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.DataGridView schedule;
     }
 }
 
--- FiberView/FiberView/Form1.cs
+++ FiberView/FiberView/Form1.cs
@@ -7,14 +7,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.Json;
+using ZXing;
+using MySql.Data.MySqlClient;
 
 namespace FiberView
 {
     public partial class Form1 : Form
     {
+        string con = "http://localhost/phpmyadmin/index.php?route=/database/structure&server=1&db=fiberview";
         public Form1()
         {
             InitializeComponent();
+            ReadAndDeserializeJson();
         }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadAndDeserializeJson();
+        }
+
+        private void ReadAndDeserializeJson()
+        {
+            try
+            {
+                string jsonFile = "fiberview.json";
+                //string directoryPath = @"C:\Users\asus\source\repos\FiberView\fiberview.json"; 
+
+                //string fullPath = Path.Combine(directoryPath, jsonFilePath);
+                string jsonData = File.ReadAllText(jsonFile);
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                var ferrySchedule = JsonSerializer.Deserialize<List<FerrySchedule>>(jsonData, options);
+
+                MessageBox.Show("Accessed");
+                
+                schedule.AutoGenerateColumns = true;
+                schedule.DataSource = ferrySchedule;
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found.");
+            }
+            catch (JsonException)
+            {
+                Console.WriteLine("Error deserializing JSON.");
+            }
+        }
+    }
+   
+    public class FerrySchedule
+    {
+        public string boat_name { get; set; }
+        public int max_capacity { get; set; }
+        public List<Departures> departures { get; set; }
+        
+    }
+    public class Departures
+    {
+        public DateTime time { get; set; }
+        public string destination { get; set; }
     }
 }
